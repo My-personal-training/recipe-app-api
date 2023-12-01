57d1ea4f184bff18ca1a4f69d8bc546bf7c852da
Lorenzo Arias Villegas <santyago.arias.sav@gmail.com>
2023-12-01 17:39:11 -0400

    Models creation and django admin page

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,8 +2,8 @@
 tests for models.
 """
 
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import TestCase
 
 
 class ModelTests(TestCase):
@@ -20,3 +20,29 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        simple_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['test2@Some.com', 'test2@some.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+        for email, expected in simple_emails:
+            user = get_user_model().objects.create_user(email, 'Testpass123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_raises_error(self):
+        """Test creating user without email raises error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'Test123')
+
+    def test_create_superuser(self):
+        """Test creating a new superuser."""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test123",
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
