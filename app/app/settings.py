a5bf868805c25c24f56a69f4dc7a2766c2b2ab24
Lorenzo Arias Villegas <santyago.arias.sav@gmail.com>
2024-01-05 22:58:30 -0400

    Checkpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'user',
     'rest_framework',
-    'drf_spectacular'
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
